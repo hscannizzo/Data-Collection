04e5a0fc1a97547866e5fb3e8c7d3e643b8097e8
hscannizzo <112523518+hscannizzo@users.noreply.github.com>
2023-03-26 20:34:55 -0400

    all i think

---
+++ info.js
@@ -0,0 +1,8 @@
+console.log("hello!");
+import csvjson from "./csvjson.json" assert{type:'json'};
+console.log(csvjson);
+const x = document.getElementsByClassName("caption");
+console.log (x)
+for (let i of x) {
+    console.log
+}
