174eccaede6389d37110fdd037206ee23e772be2
hscannizzo <112523518+hscannizzo@users.noreply.github.com>
2023-03-28 19:34:44 -0400

    SLAYYYYYINGGG JAVASCRIPT

--- audio1.js
+++ audio1.js
@@ -1,12 +1,73 @@
-const waveFrame = document.getElementById('beach2frame')
-const waveAudio = document.getElementById('beach2audio')
+const beach2Frame = document.getElementById('beach2frame')
+const beach2Audio = document.getElementById('beach2audio')
 
-waveFrame.addEventListener('mouseenter', function() {
+beach2Frame.addEventListener('mouseenter', function() {
   // Add the line below if you want the audio to start from the beginning everytime you play
- waveAudio.currentTime = 0
-  waveAudio.play()
+ beach2Audio.currentTime = 0
+  beach2Audio.play()
 })
 
-waveFrame.addEventListener('mouseleave', function() {
-  waveAudio.pause()
+beach2Frame.addEventListener('mouseleave', function() {
+  beach2Audio.pause()
+})
+
+
+
+const beach6Frame = document.getElementById('beach6frame')
+const beach6Audio = document.getElementById('beach6audio')
+
+beach6Frame.addEventListener('mouseenter', function() {
+  // Add the line below if you want the audio to start from the beginning everytime you play
+ beach6Audio.currentTime = 0
+  beach6Audio.play()
+})
+
+beach6Frame.addEventListener('mouseleave', function() {
+  beach6Audio.pause()
+})
+
+
+
+
+const beach8Frame = document.getElementById('beach8frame')
+const beach8Audio = document.getElementById('beach8audio')
+
+beach8Frame.addEventListener('mouseenter', function() {
+  // Add the line below if you want the audio to start from the beginning everytime you play
+ beach8Audio.currentTime = 0
+  beach8Audio.play()
+})
+
+beach8Frame.addEventListener('mouseleave', function() {
+  beach8Audio.pause()
+})
+
+
+
+const beach9Frame = document.getElementById('beach9frame')
+const beach9Audio = document.getElementById('beach9audio')
+
+beach9Frame.addEventListener('mouseenter', function() {
+  // Add the line below if you want the audio to start from the beginning everytime you play
+ beach9Audio.currentTime = 0
+  beach9Audio.play()
+})
+
+beach9Frame.addEventListener('mouseleave', function() {
+  beach9Audio.pause()
+})
+
+
+
+const beach13Frame = document.getElementById('beach13frame')
+const beach13Audio = document.getElementById('beach13audio')
+
+beach13Frame.addEventListener('mouseenter', function() {
+  // Add the line below if you want the audio to start from the beginning everytime you play
+ beach13Audio.currentTime = 0
+  beach13Audio.play()
+})
+
+beach13Frame.addEventListener('mouseleave', function() {
+  beach13Audio.pause()
 })
